5aef3da98b8ab58172fc2831e14979f1fb979124
Ricardo Lopez Becerra <bettermercury@comunidad.unam.mx>
2023-05-30 19:44:48 -0600

    script para monitorear los cambios

---
+++ monitor.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+# Set the directory to monitor
+DIR="/var/www/html/images"
+
+# Set the output file
+OUTFILE="/var/www/html/registro.log"
+echo "version 3"
+# Initialize last modified file
+LASTMODFILE=""
+
+# Monitor the directory for create, modify and delete events
+inotifywait -m -e create -e modify -e delete --format '%T %:e %f' --timefmt '%F %T' "$DIR" | while read -r LINE; do
+    # Parse the line into timestamp, event and file
+    TIMESTAMP=$(echo "$LINE" | cut -d ' ' -f 2)
+    EVENT=$(echo "$LINE" | cut -d ' ' -f 3)
+    FILE=$(echo "$LINE" | cut -d ' ' -f 4)
+
+    # Check if this is a modify event for the same file as the last one
+    if [ "$EVENT" = "MODIFY" ] && [ "$FILE" = "$LASTMODFILE" ]; then
+        # Ignore this event
+        continue
+    fi
+
+    # Write the line to the output file
+    echo "$LINE" >> "$OUTFILE"
+
+    # Update last modified file
+    if [ "$EVENT" = "MODIFY" ]; then
+        LASTMODFILE="$FILE"
+    else
+        LASTMODFILE=""
+    fi
+done &
--- monitoreo.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-DIRECTORY_TO_WATCH="/var/www/html/images"
-LOG_FILE="/var/www/html/registro.log"
-
-inotifywait -m -r -e modify -e create --format '%w%f' "${DIRECTORY_TO_WATCH}" | while read MODFILE
-do
-    echo "$(date) ${MODFILE} fue modificado o creado" >> "${LOG_FILE}"
-done
